5fd982983dd07d1fcce31d52be832150444797e2
WP Funnel Hackers <68546122+wp-funnel-hackers@users.noreply.github.com>
2020-08-09 15:51:33 +0800

    Create remove-all-dashboard-widgets.php

---
+++ remove-all-dashboard-widgets.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Removal All Dashboard Widgets
+ * Plugin URI: https://wpfunnelhackers.com/wordpress/plugins
+ * Description: Removal All Dashboard Widgets
+ * Version: 1.0.0
+ * Author: WP Funnel Hackers
+ * Author URI: https://wpfunnelhackers.com
+ * License:GPL2
+ *
+ */
+ 
+function remove_dashboard_widgets() {
+	global $wp_meta_boxes;
+	unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press'] );
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links'] );
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now'] );
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins'] );
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts'] );
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments'] );
+	unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_primary'] );
+	unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary'] );
+	remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
+}
+add_action( 'wp_dashboard_setup', 'remove_dashboard_widgets' ); 
